e548c2656f27f9c69d043446d66fa307f6fd0aa2
Aman Prajapati <aman15prajapati@gmail.com>
2026-08-11 19:59:17 +0530

    deployed

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const defaultOrigins = [
-  "https://blog-generator-jet.vercel.app",
+  "https://blog-generator-c4hg.vercel.app/",
   "http://localhost:3000",
 ];
 
